8c46d1a366e6d0b10fbd23056673611e767dab4d
Md Mehedi Hasan <87538600+rcmehedi15@users.noreply.github.com>
2023-01-27 11:11:54 +0600

    math needed

---
+++ Day -1/math.js
@@ -0,0 +1,22 @@
+/*
+    Math needed to start Larning a programming language
+
+   1. Number ==> 55, 999,-33
+   2. integer,odd ,even : 2,5
+   3. Float: decimal: 2.5,13.67,32.69
+   4.operation: addition,substraction,multiplication,division,remainder
+   5.percent
+   6.average
+   7. comparison : >,<, ==, !=, >=, <=
+   8. oikik: 2 kilo apple and 5 kilo mango:
+   9. 54,24,88,90,2: max,min,avg,sum
+   10.series
+   :::: Extra::::
+   1. prime
+   2. permutation
+   3. Area
+    ::: Always :::
+    
+    learning and more learning
+
+*/  -->
---
+++ Day -1/variable.js
@@ -0,0 +1,14 @@
+// Vari + able = Variable (stroage house)
+
+// Numeric 
+var mehedi = 11
+var num  = 31;
+
+// String 
+var person = "Mehedi Hasan";
+var location = "sarshi"
+
+
+// Boolean 
+var serious = true
+var isFullmarks = false

f0ff80b5e56e1d40ed015f8536fcc603c9c12a0d
Md Mehedi Hasan <87538600+rcmehedi15@users.noreply.github.com>
2023-01-28 21:58:47 +0600

    day 2 done

---
+++ Day-2/multistage-Condition.js
@@ -0,0 +1,17 @@
+var myMoney = 100;
+var danish = 45;
+var butten = 34;
+var toastbiscuit = 20;
+
+if (danish > myMoney) {
+    console.log("Danish Khamu")
+}
+else if (butten > myMoney) {
+    console.log("Butten Khamu")
+}
+else if (toastbiscuit > myMoney) {
+    console.log("toastbiscuit khamu")
+}
+else {
+    console.log("Khali Cha Khamu")
+}
---
+++ Day-2/nested-condition.js
@@ -0,0 +1,19 @@
+var math = true;
+var geomatry = true;
+var straightline = false;
+
+if (math == true) {
+    if (geomatry == true) {
+        if (straightline == true)
+        {
+            console.log('kana ')
+        }    
+            else {
+            console.log('Baka potha cholba na')
+        }
+        
+        }
+        else {
+            console.log('pitha gorous hoite parba na')
+        }
+    }

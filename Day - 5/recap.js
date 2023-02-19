59d1ea06eb706244c1c4f9fe6a99383901a61195
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-02-19 11:02:35 +0600

    dom recap

---
+++ Day - 5/recap.js
@@ -0,0 +1,21 @@
+ /*
+ 1.  how to use js with html and css
+ 2. get element or elements from dom
+  - getElementsById
+  - getElementsByClassName
+  - querySelector
+  - querySelectorAll
+3. dynamically add element to the dom : document.createElement
+ - set value to the innerHTML
+
+ 4. get and set value from a dom element
+    - use innerText (h1,p,h5,div,span,strong)
+    - use value(input,textArea)
+ 5. add event listner(click/event handler)
+ - click,focus,blur,onchange,keyup
+ - addEventListener onclick="myHandler()"
+ 6. use function to reduce duplicate code
+ 7. event bubble and event delegate
+ */
+document.querySelectorAll('users-container .disable h1');
+document.querySelectorAll('.my-users li')
